d893a321151fe97f74ec84cadb18815e24997a7e
gabriel Carino <gabejcarino@gmail.com>
2022-05-11 21:24:11 -0500

    commit tree a2c90acb25ecb6b38a7e7a6237762caee7af24b8

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [initPlants, setInitPlants] = useState([]);
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(r => r.json())
+      .then(plantsData => {
+        setInitPlants(plantsData) 
+        setPlants(plantsData)
+      })
+  }, []);
+
+  function handleSearch(search) {
+    const plantQuery = initPlants.filter(plant => plant.name.toLowerCase().includes(search));
+    setPlants(plantQuery);
+  }
+
+  function handleAddPlant(newPlant) {
+    const updatedPlants = [...plants, newPlant];
+    setPlants(updatedPlants);
+  };
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search onSearch={handleSearch} />
+      <PlantList plants={plants} />
     </main>
   );
 }
